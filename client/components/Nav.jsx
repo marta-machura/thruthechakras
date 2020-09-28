a4614e0b6d7d81a14ba42a96be45242292dc0c8d
Marta Machura <marta.machura@hotmail.com>
2020-09-28 13:16:35 +1300

    security fixes

---
+++ client/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Nav extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> Nav placeholder </h1>
+      </div>
+    )
+  }
+}
+
+export default Nav
